2c8fc2a38591996e0727f801c59362a2a1717e7c
omkar022 <omkarjadhav0199@gmail.com>
2022-10-17 09:29:25 +0530

    a

--- scripts/index.js
+++ scripts/index.js
@@ -10,19 +10,22 @@ const htmlTaskContent = ({
     description,
     type,
     url
-}) => `<div class='col-md-6 col-lg-4 mt-3 id=${id} key=${id} > 
-<div class='card shadow-sm task__card'>
-<div class='card-header d-flex gap-2 justify-content-end task__card__header'>
-<button type='button' class='btn btn-outline-info mr-2' name=${id}>
-<i class='fas fa-pencil-alt' name=${id}> </i>
-</button>
-<button type='button' class='btn btn-outline-danger mr-2' name=${id}>
-<i class='fas fa-trash-alt' name=${id}> </i>
-</button>
-</div>
-<div class='card-body'>
-${url && `<img width='100%' src=${url} alt='card image cap' class='card-image-top md-3 rounded=lg'/>`
-
+}) =>
+    `<div class='col-md-6 col-lg-4 mt-3 id=${id} key=${id} > 
+    <div class='card shadow-sm task__card'>
+    <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
+    <button type='button' class='btn btn-outline-info mr-2' name=${id}>
+    <i class='fas fa-pencil-alt' name=${id}> </i>
+    </button>
+    <button type='button' class='btn btn-outline-danger mr-2' name=${id}>
+    <i class='fas fa-trash-alt' name=${id}> </i>
+    </button>
+    </div>
+    <div class='card-body'>
+${url ?
+        `<img width='100%' src=${url} alt='card image cap' class='card-image-top md-3 rounded=lg'/>`
+        :
+        `<img width='100%' src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png" alt='card image cap' class='img-fluid place__holder__image mg-3/>`
     }
 <h4 class='task__card__title'>${title}</h4>
 <p class='description trim-3-lines text-muted' data-gram_editor='false'>
@@ -31,33 +34,42 @@ ${description}
 <div class='tags text-white d-flex flex-wrap'>
 <span class='badge bg-primary m-1'>${type}</span>
 </div>
-</div>
-<div class='card-footer'>
-<button type='button' class='btn btn-outline-primary float-right' data-bs-toggle='modal' data-bs-target='#showTask'>
-Open Task
+</div >
+    <div class='card-footer'>
+        <button 
+        type='button'
+         class='btn btn-outline-primary float-right'
+         data-bs-toggle='modal' 
+         data-bs-target='#showTask' 
+         id= ${id} 
+         onclick='openTask.apply(this,argument)'
+         >
+            Open Task
 
-</button>
-</div>
-</div>
- </div>
-`;
+        </button>
+    </div>
+</div >
+ </div >
+    `;
 
 const htmlModalContent = ({ id, title, description, url }) => {
     const date = new Date(parseInt(id));
     return `
-    
-    <div id=${id}>
-    ${url &&
-        `<img width='100%' src=${url} alt='card image cap' class='img-fluid place__holder__image mg-3/>`
+
+    < div id = ${id}>
+        ${url ?
+            `<img width='100%' src=${url} alt='card image cap' class='img-fluid place__holder__image mg-3/>`
+            :
+            `<img width='100%' src="https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png" alt='card image cap' class='img-fluid place__holder__image mg-3/>`
         }
         <strong class='text-sm text-muted'>
         Created on ${date.toDateString()}
         </strong>
         <h2 class='my-3' >${title}</h2>
         <p class ='lead'>${description}</p>
-    </div>
-    
-    
+    </div >
+
+
     `;
 };
 
@@ -75,7 +87,7 @@ const LoadInitialData = () => {
 };
 
 const handleSubmit = (event) => {
-    const id = `${Date.now()}`;
+    const id = `${Date.now()} `;
     const input = {
         url: document.getElementById('imageUrl').value,
         title: document.getElementById('taskTitle').value,
@@ -92,4 +104,10 @@ const handleSubmit = (event) => {
     );
     state.taskList.push({ ...input, id });
     updateLocalStorage();
+};
+
+const openTask = (e) => {
+    if (!e) e = window.event;
+    const getTask = state.taskList.find(({ id }) => id === e.target.id);
+    taskModal.innerHTML = htmlModalContent(getTask);
 };
